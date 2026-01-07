69b4103e6828bf46740907eac3dc85d13ae8b7bc
Ruslan Gainutdinov <ruslan@wizecore.com>
2026-01-07 19:42:18 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             >
               <Image
                 className="dark:invert"
-                src="https://dollardeploy.com/logo.svg"
+                src="https://dollardeploy.com/logo-main.svg"
                 alt="DollarDeploy logomark"
                 width={32}
                 height={32}
