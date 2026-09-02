074f478a725e10a71c302b06a4a3cd335cab6403
Ruslan Gainutdinov <ruslan@wizecore.com>
2026-09-02 08:02:02 +0300

    fix: update nextjs etc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  experimental: {
+    webVitalsAttribution: ["CLS", "LCP"],
+    serverSourceMaps: true,
+    isolatedDevBuild: false
+  },
+  devIndicators: false,
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/logging
+  logging: {
+    incomingRequests: false,
+    fetches: {
+      fullUrl: true
+    }
+  }
 };
 
 export default nextConfig;
